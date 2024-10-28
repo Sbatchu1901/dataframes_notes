12fa29b42178c1cf0c9182f185a324823a6ec02c
sbatchu1901 <sbatchu@murraystate.edu>
2024-10-28 11:03:45 -0500

    q1

--- 01_create_empty.py
+++ 01_create_empty.py
@@ -3,3 +3,5 @@ import functions
 df1 = pd.DataFrame()
 functions.print_it('Empty dataframe', df1)
 # create another empty dataframe and print it <<< TO DO
+df2=pd.DataFrame()
+functions.print_it('second data frame',df2)
