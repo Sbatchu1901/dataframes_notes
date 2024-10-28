fd5d9cdc3eba7e4b0a3e1ca3c97f87eac1cdf808
sbatchu1901 <sbatchu@murraystate.edu>
2024-10-28 11:50:38 -0500

    hjhj

--- 07_create_from_sql.py
+++ 07_create_from_sql.py
@@ -7,5 +7,11 @@ my_cursor = connection.cursor()
 countries = my_cursor.execute ('select * from countries')
 rows= my_cursor.fetchall()
 countries_df = pd.DataFrame(rows)
+countries_df.columns= ['country','continent']
 functions.print_it('df from query result: ', countries_df)
 # create a dataframethat shows sales data from the sales table <<< TO DO
+countries = my_cursor.execute ('select * from countries')
+rows= my_cursor.fetchall()
+sales_df = pd.DataFrame(rows)
+countries_df.columns= ['country','continent']
+functions.print_it('df from query result: ', sales_df)
--- 08_create_from_sql2.py
+++ 08_create_from_sql2.py
@@ -6,3 +6,5 @@ connection = db.connect(file_name)
 countries_df2 = pd.read_sql('Select * from countries', connection)
 functions.print_it('df directly from query result: ', countries_df2)
 # create a dataframe that uses sales data and stores it to a dataframe <<<< TO DO
+sales_df=pd.read_sql('select * from sales',connection)
+functions.print_it('mysales df',sales_df)
